5d002bc304df036786595c6606cbf590c0378ab6
Filip Parag <filip@parag.rs>
2022-12-22 19:40:41 +0100

    interactive prompt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,89 @@
 #![feature(unchecked_math)]
 
+use processor::Processor;
 mod asm;
 mod error;
 mod instructions;
 mod processor;
 
-fn main() {}
+fn prompt(name: &str) -> Option<Vec<String>> {
+    use std::io::Write;
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    match std::io::stdin().read_line(&mut line) {
+        Ok(_) => Some(line.trim().split(' ').map(str::to_string).collect()),
+        Err(_) => None,
+    }
+}
+
+fn main() {
+    let mut p = Processor::new();
+
+    p.load_ram(&asm::DATA_MEMORY);
+    // p.load_rom_str(&asm::ROM_BIN).unwrap();
+    p.load_rom(&asm::ASSEMBLY_CODE);
+
+    let mut print_always: bool = true;
+    println!("{}", p);
+    loop {
+        if let Some(input) = prompt("lprsemu >> ") {
+            if input.len() == 0 {
+                continue;
+            }
+            if print_always {
+                print!("{}[2J", 27 as char);
+            }
+            match input[0].as_str() {
+                "p" | "print" => println!("{}", p),
+                "pa" | "print auto" => {
+                    print_always = !print_always;
+                    println!("Auto-print: {}", print_always);
+                }
+                "r" | "run" => {
+                    p.run(true);
+                    if print_always {
+                        println!("{}", p)
+                    }
+                }
+                "ra" | "run all" => {
+                    p.run(false);
+                    if print_always {
+                        println!("{}", p)
+                    }
+                }
+                "s" | "step" | "" => {
+                    p.tick();
+                    if print_always {
+                        println!("{}", p)
+                    }
+                }
+                "b" | "breakpoint" => {
+                    if input.len() != 2 {
+                        println!("Argument error");
+                        continue;
+                    }
+                    let line: usize = input[1].parse().expect("Parsing error");
+                    p.toggle_breakpoint(line);
+                    if print_always {
+                        println!("{}", p)
+                    }
+                }
+                "j" | "jump" => {
+                    if input.len() != 2 {
+                        println!("Argument error");
+                        continue;
+                    }
+                    let line: usize = input[1].parse().expect("Parsing error");
+                    p.toggle_breakpoint(line);
+                    if print_always {
+                        println!("{}", p)
+                    }
+                }
+                _ => println!("Command error"),
+            }
+        } else {
+            println!("Input error");
+        }
+    }
+}
