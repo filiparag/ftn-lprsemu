c5f2d8508fe1d69ac0cc2858ce27216a2746be6d
Filip Parag <filip@parag.rs>
2023-01-21 15:11:01 +0100

    add example comments

--- example.asm
+++ example.asm
@@ -1,22 +1,23 @@
+#!/usr/bin/env lprsemu
+
+/* Signed multiplication example */
+
 .data
-0x0000
-5, 6
+    0x000               // Result will be stored here
+    5, -6               // Multiplication factors
 
 .text
-main:
-    inc   R0, R0
-    ld    R1, R0
-    inc   R0, R0
-    ld    R2, R0
-    sub   R0, R0, R0
+load:
+    inc   R0, R0        // Set index to 1
+    ld    R1, R0        // Load memory at index 1 (first factor)
+    inc   R0, R0        // Set index to 2
+    ld    R2, R0        // Load memory at index 2 (second factor)
+    sub   R0, R0, R0    // Prepare accumulator
 loop:
-    add   R0, R0, R1
-    dec   R2, R2
-    jmpnz loop
-    st    R0, R2
-shift:
-    shr   R0, R0
-    jmpnz shift
-divide:
-    shl   R1, R1
-    jmpnz divide
+    add   R0, R0, R1    // Add first factor to accumulator
+    dec   R2, R2        // Decrease repetition counter (second factor)
+    jmpnz loop          // Repeat if counter is greater than zero
+store:
+    st    R0, R2        // Store result to memory
+end:
+    jmp end             // Loop forever
