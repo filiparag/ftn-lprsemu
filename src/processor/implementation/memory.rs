16ca0a493b161dd6f24aa0f523e5010b009d85b2
Filip Parag <filip@parag.rs>
2023-01-23 13:53:17 +0100

    set flags on memory instructions

--- src/processor/implementation/memory.rs
+++ src/processor/implementation/memory.rs
@@ -12,12 +12,16 @@ impl Processor {
                 in_range![REG_COUNT; z, y];
                 in_range![RAM_SIZE; reg![self; y]];
                 reg![self; z] = mem![self; reg![self; y]];
+                self.flags.zero = reg![self; z] == 0;
+                self.flags.sign = reg![self; z] & 0x8000 != 0;
                 Ok(())
             }
             MemoryInstruction::Store(x, y) => {
                 in_range![REG_COUNT; x, y];
                 in_range![RAM_SIZE; reg![self; y]];
                 mem![self; reg![self; y]] = reg![self; x];
+                self.flags.zero = reg![self; x] == 0;
+                self.flags.sign = reg![self; x] & 0x8000 != 0;
                 Ok(())
             }
         }
